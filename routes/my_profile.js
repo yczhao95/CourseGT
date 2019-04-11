a43c3872caf06554981bcfd8649a17b00650801c
Yuhao Lan <yuhaolan@gatech.edu>
2019-04-11 04:55:59 +0000

    finished degree track

--- routes/my_profile.js
+++ routes/my_profile.js
@@ -3,9 +3,26 @@ var router  = express.Router();
 var User    = require("../models/user");
 var passport    = require("passport");
 var Course  = require("../models/course");// this is for courses database setup
+var fs = require("fs");
+
+var json_data;
+
+
+
+fs.readFile("/home/ubuntu/workspace/CourseGT/views/profiles/specification.json", 
+             {"encoding": "utf8"}, 
+              function(err, data) {
+     if (err)
+        console.log(err);
+     else {
+        json_data = JSON.parse(data);  
+        console.log(json_data);
+
+     }
+ });
 
 router.get("/myprofile", function(req, res){
-    res.render("profiles/myprofile");
+    res.render("profiles/myprofile", {json_data: json_data});
 });
 
 router.post("/myprofile", function(req, res){
